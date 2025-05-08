cc7ae9b578a2d49542c29f627176648a9e163d3e
carnesjon7 <carnesjon7@gmail.com>
2025-05-08 15:51:00 -0500

    Add files via upload

--- mynews_ai_backend.py
+++ mynews_ai_backend.py
@@ -5,3 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return 'MyNews AI is live.'
+# Don't include app.run() anymore
+# Just define 'app' like this at the top of the file:
+# from flask import Flask
+# app = Flask(__name__)
+
+# Nothing else needed at the bottom
